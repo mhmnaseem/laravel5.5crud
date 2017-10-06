9aed4ebc7ef1a76a26c8bc0a2a8df1cf6d1afba6
Mahmud Naseem <mhmnaseem@gmail.com>
2017-10-06 10:58:14 +0530

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                  Welcome to video Library
+                  Welcome to Video Library
                 </div>
 
                 <div class="links">
